1442c6d38e72a3fb02ce012671784f1ca9103dd3
Landon Conover <landon.conover@gmail.com>
2014-10-29 15:26:23 -0600

    Added the angular route provider. And added templates. [axof: 116]

--- app/js/app.js
+++ app/js/app.js
@@ -3,9 +3,56 @@
 *
 * Description
 */
-angular.module('RecipeApp', ['recipeservices']).
+recipeApp = angular.module('RecipeApp', ['recipeservices','ngRoute']);
 
-controller('RecipeController', function($scope, Recipe){
+
+
+// Define Routing for app
+recipeApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+      }).
+      when('/recipes', {
+        templateUrl: 'templates/recipes.html',
+        controller: 'RecipeController'
+      }).
+      when('/planner', {
+        templateUrl: 'templates/planner.html',
+        controller: 'PlannerController'
+      }).
+      when('/shopping', {
+        templateUrl: 'templates/list.html',
+        controller: 'ListController'
+      }).
+      when('/addRecipe', {
+        templateUrl: 'templates/addRecipe.html',
+        controller: 'AddRecipeController'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+}]);
+
+
+recipeApp.controller('HomeController', function($scope, Recipe){
+
+});
+
+recipeApp.controller('RecipeController', function($scope, Recipe){
 	$scope.recipes = Recipe.query();
-})
+});
 
+recipeApp.controller('PlannerController', function($scope, Recipe){
+
+});
+
+recipeApp.controller('ListController', function($scope, Recipe){
+
+});
+
+recipeApp.controller('AddRecipeController', function($scope, Recipe){
+
+});

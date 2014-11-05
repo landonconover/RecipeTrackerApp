ac0dab9acbed19ffb559c0309db62a082b4bef04
Brittany <brittonyas@aol.com>
2014-11-04 22:59:59 -0700

    adding Javascript to get add row button to work./ updating html to work with javascript. [axof: 137]

---
+++ app/js/addrow.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+// add button to work
+var rowNum = 0;
+function addRow(frm) {
+rowNum ++;
+var row = '<p id="rowNum'+rowNum+'">Qty <input type=number" name="qty[]" size="4" value="'+frm.add_qty.value+'"> Ingredient name: <input type="text" name="name[]" value="'+frm.add_name.value+'"> <input type="button" value="Remove" onclick="removeRow('+rowNum+');"></p>';
+jQuery('#itemRows').append(row);
+frm.add_qty.value = '';
+frm.add_name.value = '';
+}
